c336426be7be6f5bcab7c6acec29fda682dee21f
stratolark <69923983+stratolark@users.noreply.github.com>
2022-10-06 16:14:26 -0300

    feat: add Tailwind CSS. feat: add tailwind classes to /apps/web. feat: add tailwind prettier plugin.

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { useEffect, useState } from "react";
 import { Button } from "ui";
 
@@ -34,32 +35,55 @@ export default function Web() {
   };
 
   return (
-    <div>
-      <h1>Web</h1>
-      <form onSubmit={onSubmit}>
-        <label htmlFor="name">Name </label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          value={name}
-          onChange={onChange}
-        ></input>
-        <Button type="submit">Submit</Button>
-      </form>
-      {error && (
-        <div>
-          <h3>Error</h3>
-          <p>{error}</p>
-        </div>
-      )}
-      {response && (
-        <div>
-          <h3>Greeting</h3>
-          <p>{response.message}</p>
-          <Button onClick={onReset}>Reset</Button>
-        </div>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Turborepo Next.js, Tailwind CSS example</title>
+      </Head>
+      <div className="m-12 text-white">
+        <h1 className="mb-4 text-xl font-bold">Web</h1>
+        <form onSubmit={onSubmit} className="grid">
+          <label htmlFor="name" className="mb-2 text-lg">
+            Enter your Name:
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            value={name}
+            onChange={onChange}
+            placeholder="Bob"
+            className="mb-4 rounded p-2.5 text-black"
+          ></input>
+          <Button
+            className="rounded-md bg-blue-500 p-4 hover:bg-blue-600"
+            type="submit"
+          >
+            Submit
+          </Button>
+        </form>
+        {error && (
+          <div className="mt-6">
+            <h3 className="text-lg font-medium text-rose-400">
+              Oops an Error occurred!:
+            </h3>
+            <p className="mt-2 rounded-md bg-red-400 p-4 text-black">{error}</p>
+          </div>
+        )}
+        {response && (
+          <div className="mt-6">
+            <h3 className="text-lg font-medium text-teal-400">Greetings:</h3>
+            <p className="text- mt-2 rounded-md bg-teal-400 p-4 text-black">
+              {response?.message ?? "Bob"}
+            </p>
+            <Button
+              className="mt-4 rounded-md bg-slate-500 p-2.5"
+              onClick={onReset}
+            >
+              Reset
+            </Button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
